4ffd6fdcf0b62ff4f3fb2ce01c1d87d195b9cc83
mustafa <debf2@ymail.com>
2023-05-02 16:30:33 +0300

    d

--- test/flutter_helper_test.dart
+++ test/flutter_helper_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_helper/flutter_helper.dart';
+import 'package:mzgs_flutter_helper/flutter_helper.dart';
 
 void main() {
   test('adds one to input values', () {
